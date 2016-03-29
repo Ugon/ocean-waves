fb62ffb5f910b7438cc3453ca74616658294aa6f
Wojciech Pachuta <wojciech.pachuta01@gmail.com>
2016-03-29 22:23:31 +0200

    fft finished

--- parameters.js
+++ parameters.js
@@ -9,11 +9,10 @@ var UNIT_TEXTURE_PING = 2;
 var UNIT_TEXTURE_PONG = 3;
 
 var TRANSFORM_SIZE = 512;
+var AREA_SIZE = 10000;
 
-var WIND_X = 12;
-var WIND_Y = 12;
-var X_SIZE = 1000;
-var Y_SIZE = 1000;
+var WIND_X = 10;
+var WIND_Y = 10;
 
 var randomNormal = function() {
     return (Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() - 3) / 3;
