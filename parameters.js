3c5e24c6375f1c199182dc1889526bfe7b47dc8d
Wojciech Pachuta <wojciech.pachuta01@gmail.com>
2016-03-30 22:18:45 +0200

    cleanup, refactoring

--- parameters.js
+++ parameters.js
@@ -1,18 +1,15 @@
-var WIDTH = 		512;
-var HEIGHT = 		512;
-
 var A_POSITION_INDEX = 0;
 
-var UNIT_TEXTURE_RANDOM_NORMAL_PAIRS = 0;
+var UNIT_TEXTURE_RANDOM_COMPLEX_NUMBERS = 0;
 var UNIT_TEXTURE_INIT_HEIGHT_IN_FREQUENCY = 1;
 var UNIT_TEXTURE_PING = 2;
 var UNIT_TEXTURE_PONG = 3;
 
 var TRANSFORM_SIZE = 512;
-var AREA_SIZE = 50;
+var AREA_SIZE = 5000;
 
 var WIND_X = 100;
-var WIND_Y = 1;
+var WIND_Y = 10;
 
 var randomNormal = function() {
     return (Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() - 3) / 3;
